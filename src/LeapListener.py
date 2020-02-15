4ebf9ac0cbf23d2efe8fa6281ceacad64a2cb9bc
beepboop271 <53926222+beepboop271@users.noreply.github.com>
2020-02-15 12:37:39 -0500

    index finger detection

--- src/LeapListener.py
+++ src/LeapListener.py
@@ -9,5 +9,8 @@ class LeapListener(Leap.Listener):
 
     def on_frame(self, controller):
         frame = controller.frame()
-        print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d" % (
-          frame.id, frame.timestamp, len(frame.hands), len(frame.fingers))
+        index = frame.hands.rightmost.fingers.finger_type(Leap.Finger.TYPE_INDEX)[0]
+        print "Extended:", index.is_extended, "Distance:", index.touch_distance, \
+              "Zone:", index.touch_zone, "Position:", index.tip_position
+        # print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d" % (
+        #   frame.id, frame.timestamp, len(frame.hands), len(frame.fingers))
