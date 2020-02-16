453d7473b0b8fc3e6c2094ae8ad45e72544e8cc6
Shari Sun <shari09cxxcr@gmail.com>
2020-02-15 19:15:46 -0500

    half working (at least doesnt break)

--- src/LeapListener.py
+++ src/LeapListener.py
@@ -3,8 +3,8 @@ sys.path.insert(0, "./lib/x86")
 import Leap
 
 
-def isDrawPos(hand):
-    for finger in hand.fingers:
+def isDrawPos(rightHand):
+    for finger in rightHand.fingers:
         if finger.type != Leap.Finger.TYPE_INDEX:
             if finger.is_extended:
                 return False
@@ -13,18 +13,41 @@ def isDrawPos(hand):
                 return False
     return True
 
+def isZoomPos(hand):
+    for finger in hand.fingers:
+        if (finger.type == Leap.Finger.TYPE_INDEX
+            or finger.type == Leap.Finger.TYPE_THUMB):
+            if not finger.is_extended:
+                return False
+        else:
+            if finger.is_extended:
+                return False
+    return True
+
 
 class LeapListener(Leap.Listener):
+
     def on_connect(self, controller):
         print 'connected'
+        self.lastFrame = controller.frame
 
     def on_frame(self, controller):
         frame = controller.frame()
         if not frame.hands.is_empty: 
             rightHand = frame.hands.rightmost
+            # drawing pos
             if isDrawPos(rightHand):
                 print 'can draw now'
                 index = rightHand.fingers.finger_type(Leap.Finger.TYPE_INDEX)[0]
+            # full fist
+            elif isZoomPos(rightHand):
+                pinchDiff = (rightHand.pinch_strength 
+                             - self.lastFrame.hands.rightmost.pinch_strength)
+                diffMark = 0.05
+                if pinchDiff > diffMark:
+                    print 'zoom out'
+                elif pinchDiff < -diffMark:
+                    print 'zoom in'
             elif rightHand.grab_strength == 1:
                 print 'settings page'
             elif rightHand.grab_strength > 0.5:
@@ -44,24 +67,24 @@ class LeapListener(Leap.Listener):
         
 
         
-        while (hand.fingers.finger_type(Finger.TYPE_INDEX) and hand.fingers.finger_type(Finger.TYPE_THUMB)):
-            if  0.5 < hand.PinchStrength <= 1:
-                print "zoom out"
-            elif 0 >= hand.PinchStrength <= 0.5:
-                print "zoom in"
+        # while (rightHand.fingers.finger_type(Leap.Finger.TYPE_INDEX) and rightHand.fingers.finger_type(Finger.TYPE_THUMB)):
+        #     if  0.5 < rightHand.PinchStrength <= 1:
+        #         print "zoom out"
+        #     elif 0 >= rightHand.PinchStrength <= 0.5:
+        #         print "zoom in"
 
-        for gesture in frame.guesture():
-            if gesture.type == Leap.Gesture.TYPE_CIRCLE:
-                circle = CircleGesture(gesture)
-                if circle.pointable.direction.angle_to(circle.normal) <= Leap.PI/2:
-                    clockwise = "clockwise"
-                else:
-                    counterclockwise = "counter-clockwise"
-                swept_angle = 0
-                if circle.state != Leap.Gesture.STATE_START:
-                    previous = CircleGesture(controller.fram(1).gesture(circle.id))
-                    swept_angle = (circle.progress - previous.progress) * 2 * Leap.PI
-                print "id: " + str(circle.id) + "progress: " + str(circle.progress) + " Radius: " + str(circle.radius) + " swept angle: " + str(swept_angle * Leap.RAD_TO_DEG) + " " + clockdirection
+        # for gesture in frame.guesture():
+        #     if gesture.type == Leap.Gesture.TYPE_CIRCLE:
+        #         circle = CircleGesture(gesture)
+        #         if circle.pointable.direction.angle_to(circle.normal) <= Leap.PI/2:
+        #             clockwise = "clockwise"
+        #         else:
+        #             counterclockwise = "counter-clockwise"
+        #         swept_angle = 0
+        #         if circle.state != Leap.Gesture.STATE_START:
+        #             previous = CircleGesture(controller.fram(1).gesture(circle.id))
+        #             swept_angle = (circle.progress - previous.progress) * 2 * Leap.PI
+        #         print "id: " + str(circle.id) + "progress: " + str(circle.progress) + " Radius: " + str(circle.radius) + " swept angle: " + str(swept_angle * Leap.RAD_TO_DEG) + " " + clockdirection
                 
 
 
